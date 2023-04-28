3bb9ed6e97046c912621ba2e8684d0f89bd6943e
kassio oliveira <kassiooliveira875@gmail.com>
2023-04-28 14:51:42 -0300

    feat: add new route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import Home from './pages/Home'
+import { Checkout } from './pages/Checkout'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   )
