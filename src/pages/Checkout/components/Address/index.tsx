cf691d9803a08bba57b90d6a2d9241f5105bf072
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-02 16:30:47 -0300

    feat: handleAddAddress function

--- src/pages/Checkout/components/Address/index.tsx
+++ src/pages/Checkout/components/Address/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import {
   AddressContainer,
   CityInput,
@@ -11,7 +12,41 @@ import {
   ZipCodeInput,
 } from './style'
 
-export default function Address() {
+import { v4 as uuidv4 } from 'uuid'
+import { format } from 'date-fns'
+
+import { ptBR } from 'date-fns/locale'
+
+import { useFormContext } from 'react-hook-form'
+import { OrdersContext } from '../../../../contexts/OrdersProvider'
+import { toast } from 'react-toastify'
+
+interface AddressProps {
+  method: string
+  handlePaymentMethod: (method: string) => void
+}
+
+export default function Address({ method, handlePaymentMethod }: AddressProps) {
+  const { register, handleSubmit, reset } = useFormContext()
+
+  const { completeCurrentOrder } = useContext(OrdersContext)
+
+  function handleAddAddress(data: any) {
+    const newOrderData = {
+      ...data,
+      paymentPreference: method,
+      date: format(new Date(), 'dd/MM/yyyy', { locale: ptBR }),
+      id: uuidv4(),
+    }
+    if (method.length === 0) {
+      toast.warning('Selecione um meio de pagamento!')
+      return
+    }
+    completeCurrentOrder(newOrderData)
+    handlePaymentMethod('')
+    reset()
+  }
+
   return (
     <AddressContainer>
       <h3>Complete seu pedido</h3>
@@ -23,24 +58,40 @@ export default function Address() {
           <p>Informe o endereço onde deseja receber seu pedido</p>
         </div>
 
-        <form>
-          <ZipCodeInput type="text" name="Zip-Code" placeholder="CEP" />
-          <RoadInput placeholder="Rua" />
+        <form onSubmit={handleSubmit(handleAddAddress)} id="completeOrderForm">
+          <ZipCodeInput type="text" placeholder="CEP" {...register('CEP')} />
+          <RoadInput placeholder="Rua" type="text" {...register('road')} />
 
           <FormContainer>
             <div>
-              <NumberInput type="number" name="number" placeholder="Numero" />
-              <ComplementInput placeholder="Complemento" />
+              <NumberInput
+                type="text"
+                {...register('number')}
+                placeholder="Número"
+              />
+              <ComplementInput
+                placeholder="Complemento"
+                type="text"
+                {...register('complement')}
+              />
             </div>
             <div>
               <NeighborhoodInput
                 type="text"
-                name="district"
                 placeholder="Bairro"
+                {...register('district')}
               />
               <div>
-                <CityInput type="text" placeholder="Cidade" />
-                <StateInput type="text" placeholder="UF" />
+                <CityInput
+                  type="text"
+                  placeholder="Cidade"
+                  {...register('city')}
+                />
+                <StateInput
+                  type="text"
+                  placeholder="UF"
+                  {...register('estate')}
+                />
               </div>
             </div>
           </FormContainer>
