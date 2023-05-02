5d1ed753648a4e501987c1c67fc9855568230864
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-02 16:25:41 -0300

    feat: add toastify

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,27 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { OrdersProvider } from './contexts/OrdersProvider'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/ReactToastify.min.css'
 
 export function App() {
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </ThemeProvider>
+      <OrdersProvider>
+        <ThemeProvider theme={defaultTheme}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <Router />
+            <ToastContainer
+              autoClose={3000}
+              pauseOnHover={false}
+              style={{ width: 'max-content' }}
+            />
+          </BrowserRouter>
+        </ThemeProvider>
+      </OrdersProvider>
     </>
   )
 }
