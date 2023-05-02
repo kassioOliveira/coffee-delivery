373f1f2b10bebf214518bb2ba3698aa5a8b22478
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-02 16:36:21 -0300

    feat: completion of CardCheckout

--- src/pages/Checkout/components/SelectedCoffees/components/CardCheckout/index.tsx
+++ src/pages/Checkout/components/SelectedCoffees/components/CardCheckout/index.tsx
@@ -1,26 +1,68 @@
 import { CardCheckoutContainer, Trash } from './style'
 
-import image from '../../../../../../assets/Image.arabe.svg'
 import { DoubleButton } from '../../../../../../components/DoubleButton'
 import { useWindowSize } from '../../../../../../hooks/useWindowSize'
 
-export default function CardCheckout() {
+import {
+  CoffeeType,
+  OrdersContext,
+} from '../../../../../../contexts/OrdersProvider'
+import { useContext, useEffect, useState } from 'react'
+
+interface CardData {
+  item: CoffeeType
+}
+
+export default function CardCheckout({ item }: CardData) {
   const size = useWindowSize()
+
+  const { updateAmountItemCart, removeCoffeeFromCart } =
+    useContext(OrdersContext)
+
+  const [currentAmount, setCurrentAmount] = useState(0)
+
+  useEffect(() => {
+    setCurrentAmount(item.amount)
+  }, [item.amount])
+
+  function handleDecrease() {
+    if (currentAmount === 1) {
+      removeCoffeeFromCart(item.id)
+      return
+    }
+    setCurrentAmount((state) => state - 1)
+
+    updateAmountItemCart(item, currentAmount - 1)
+  }
+
+  function handleIncrease() {
+    if (currentAmount === 9) {
+      return
+    }
+    setCurrentAmount((state) => state + 1)
+    updateAmountItemCart(item, currentAmount + 1)
+  }
 
   return (
     <CardCheckoutContainer>
-      <img src={image} alt="" />
+      <img src={item.srcImg} alt={item.title} />
       <div>
-        <span>Expresso Tradicional</span>
+        <span>{item.title}</span>
         <div>
-          <DoubleButton resize />
-          <button>
+          <DoubleButton
+            id={item.id}
+            amount={currentAmount}
+            increase={handleIncrease}
+            decrease={handleDecrease}
+            resize
+          />
+          <button onClick={() => removeCoffeeFromCart(item.id)}>
             <Trash />
             {size.width && (size.width > 400 ? 'Remover' : '')}
           </button>
         </div>
       </div>
-      <strong>R$ 9,90</strong>
+      <strong>R$ {item.price}</strong>
     </CardCheckoutContainer>
   )
 }
