b9caadc27f58a7cd96cc442b19dc52b835879380
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-02 11:48:13 -0300

    feat: decrease and increase functionality

--- src/components/DoubleButton/index.tsx
+++ src/components/DoubleButton/index.tsx
@@ -2,16 +2,34 @@ import { DoubleButtonContainer, Minus, Plus } from './style'
 
 interface IButtonsResize {
   resize?: Boolean
+  id: string
+  amount: number
+  increase: (id: string) => void
+  decrease: (id: string) => void
 }
 
-export function DoubleButton({ resize }: IButtonsResize) {
+export function DoubleButton({
+  resize,
+  id,
+  amount,
+  increase,
+  decrease,
+}: IButtonsResize) {
+  function handleIncrease() {
+    increase(id)
+  }
+
+  function handleDecrease() {
+    decrease(id)
+  }
+
   return (
     <DoubleButtonContainer resize={!!resize}>
-      <button type="button">
+      <button type="button" onClick={handleDecrease}>
         <Minus />
       </button>
-      <strong>1</strong>
-      <button type="button">
+      <strong>{amount}</strong>
+      <button type="button" onClick={handleIncrease}>
         <Plus />
       </button>
     </DoubleButtonContainer>
