826cb132b3a04f2bc3f4da616beeaa4e8b1f4fe5
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-03 11:07:31 -0300

    feat: new route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import Home from './pages/Home'
 import { Checkout } from './pages/Checkout'
+import { Success } from './pages/Success'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
